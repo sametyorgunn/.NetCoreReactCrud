6e28f94548c430cab2d6d1dcda059bba5593d809
samet <74676437+sametyorgunn@users.noreply.github.com>
2024-02-19 00:45:22 +0300

    ReactCrudAppNetCore
    ReactCrudAppNetCore

---
+++ crudWithReact/crudWithReact/Controllers/BookController.cs
@@ -0,0 +1,57 @@
+﻿using crudWithReact.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crudWithReact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           var books = _context.Books;
+           return Ok(books);
+        }
+        [HttpGet("Get-ById")]
+        public IActionResult GetById(int id)
+        {
+            var book = _context.Books.Find(id);
+            return Ok(book);
+        }
+
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return Ok(200);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges(); 
+            return Ok(200);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            _context.Remove(book);
+            _context.SaveChanges();
+            return Ok(200);
+        }
+    }
+}
---
+++ crudWithReact/crudWithReact/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace crudWithReact.Models
+{
+    public class AppDbContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Book>Books { get; set; }
+    }
+}
